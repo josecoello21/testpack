088a94742f6c9b2dc07968e0321cb4773bd748a5
josecoello21 <josemanuelcoello@gmail.com>
2021-03-17 11:02:18 -0400

    test fuction

--- test/test.R
+++ test/test.R
@@ -1,5 +1,5 @@
 library(testthat)
-library(magrittr)
+library(tidyverse)
 
-expect_that(fars_summarize_years(2013) %>% dim(), equals(c(12,2))) %>% 
+expect_that(fars_summarize_years(2013) %>% dim(), equals(c(12,2))) %>%
   suppressMessages()
