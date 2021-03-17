91334b06dece50253e2cfb75ec12373307d70372
josecoello21 <josemanuelcoello@gmail.com>
2021-03-17 14:54:14 -0400

    test

--- test/test.R
+++ test/test.R
@@ -1,5 +1,8 @@
 library(testthat)
 library(tidyverse)
+source(file.path(getwd(), 'R', 'fars_functions.R'))
+
+setwd(file.path(getwd(), 'test'))
 
 expect_that(fars_summarize_years(2013) %>% dim(), equals(c(12,2))) %>%
   suppressMessages()
